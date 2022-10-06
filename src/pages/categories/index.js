fdf19f23192a5a276826b5827281be32e8d55e9b
Simeon Duwel <39743643+WalrusGumboot@users.noreply.github.com>
2022-10-06 10:28:51 +0200

    bruhhh

--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -21,7 +21,7 @@ const CategoriesPage = ( {data} ) => {
             <div className="gap-y-12">
             {
                 categories.map( cat => (
-                    <div className="bg-emerald-100 hover:shadow-md hover:bg-emerald-400 p-4 rounded-md hover:cursor-pointer w-full transition-all"
+                    <div className="bg-emerald-100 hover:shadow-md hover:bg-emerald-400 p-4 mb-4 rounded-md hover:cursor-pointer w-full transition-all"
                      onClick={ () => navigate("/category/"+cat.title)} role="link">
                         <h3 className="text-3xl mb-4">{cat.title}</h3>
                         <p>{cat.description}</p>
