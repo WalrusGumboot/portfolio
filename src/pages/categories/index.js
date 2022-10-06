0219fa2dda09ed4deff827a96ce3774846076120
Simeon Duwel <39743643+WalrusGumboot@users.noreply.github.com>
2022-10-06 10:21:03 +0200

    bruh

--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -4,21 +4,23 @@ import Frame from "../../components/frame"
 
 export const query = graphql `
 query CategoryQuery {
-    allCategory {
-        title
-        description
+    allSanityCategory {
+        nodes {
+            title
+            description
+        }
     }
 }
 `
 
 const CategoriesPage = ( {data} ) => {
-    const categories = data.allCategory;
+    const categories = data.nodes.allCategory;
 
     return (
         <Frame>
             <div className="gap-y-12 flex flex-column">
             {
-                categories.map( (cat) => (
+                categories.map( cat => (
                     <div className="bg-emerald-200 hover:shadow-md hover:bg-emerald-400 w-full transition-all"
                      onClick={ () => navigate("/category/"+cat.title)} role="link">
                         <h3 className="text-xl">{cat.title}</h3>
